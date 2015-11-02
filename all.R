c726f58af2452100d41eea48bd63a460ee71bd20
mikolajjj <mikolajjj@gmail.com>
2015-11-02 17:37:45 +0100

    drobne korekty

--- all.R
+++ all.R
@@ -7,9 +7,9 @@ all <- rbind(df_1K,df_2K,df_3K,df_4K,df_5K,df_6K,df_7K)
 #nazwy kolumn
 colnames(all) <- c("kadencja", "etap", "osoba", "klub")
 
-#usunięcie pozotsałych przypisów
+#usunięcie pozostałych przypisów
 all$klub <- str_replace_all(all$klub, "\\[.\\]|\\[..\\]", "")
 
 #eksport danych do plików
 save(all, file="all.rda")
-write.csv(all, file="all.csv", sep=";")
+write.csv(all, file="all.csv")
